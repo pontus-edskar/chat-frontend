5ec21a0939ae67d0f05286b43bfd16554bee1833
pontus-edskar <95104730+pontus-edskar@users.noreply.github.com>
2025-05-03 00:05:49 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChatHome from './ChatHome';
+
+function App() {
+    return (
+        <div className="App">
+            <ChatHome />
+        </div>
+    );
+}
+
+export default App;
